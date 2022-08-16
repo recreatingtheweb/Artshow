c4c5e44859adbcdf1cc5e0cbac2bc30ececaad50
recreatingtheweb <recreatingtheweb@gmail.com>
2022-08-16 13:21:46 -0400

    Home.js
    Fixed call to action positioning.

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ function Home(){
     return <div >
         {/* <Header title="Welcome to my mock photography display!"/> */}
         <div className="call-to-action">
-        <h1 style = {{  marginBottom : 40 }}> Mock Portfolio</h1>
+        <h1 > Mock Portfolio</h1>
         <Link to="/portfolio" style = {{  marginRight : 15 }} class="btn btn-lg btn-secondary fw-bold border-white bg-dark"><i class="fa-solid fa-circle-arrow-right"></i> Portfolio</Link>
         <Link to="/contact" style = {{  marginLeft : 15 }} class="btn btn-lg btn-secondary fw-bold border-white bg-dark" ><i class="fa-solid fa-book-open"></i> Contact </Link>
         </div>
